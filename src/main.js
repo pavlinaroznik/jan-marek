f7334ca6701f25c32fdc6ba9c48a9506c65910d3
PavlinaZednikova <zednikovapav@gmail.com>
2025-07-27 18:16:46 +0200

    first drop down implemented

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
-import './style.css'
+  document.querySelectorAll('.dropdown-header').forEach(button => {
+    button.addEventListener('click', () => {
+      const content = button.nextElementSibling;
 
+    //   // Collapse all others (optional)
+    //   document.querySelectorAll('.dropdown-content').forEach(el => {
+    //     if (el !== content) {
+    //       el.style.maxHeight = null;
+    //     }
+    //   });
+
+      // Toggle current
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  });
